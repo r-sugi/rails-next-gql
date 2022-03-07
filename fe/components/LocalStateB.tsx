e18a81d70416b71432445afbe839601db00a1a77
r-sugi <r.sugimoto0615@gmail.com>
2022-03-07 16:51:15 +0900

    fe update:sample page/component for makevar

---
+++ fe/components/LocalStateB.tsx
@@ -0,0 +1,22 @@
+import { VFC } from 'react'
+import { userVar } from '@@/cache'
+import { useReactiveVar } from '@apollo/client'
+import Link from 'next/link'
+
+export const LocalStateB: VFC = () => {
+  const users = useReactiveVar(userVar)
+  return (
+    <>
+      {users.map((user, index) => {
+        return (
+          <p className="mb-3" key={index}>
+            {user.name}
+          </p>
+        )
+      })}
+      <Link href="/local-state-a">
+        <a>Back</a>
+      </Link>
+    </>
+  )
+}
