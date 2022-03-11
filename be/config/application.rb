e7b1ed9669da70888cf32dea31dcd6c6499e3b6d
r-sugi <r.sugimoto0615@gmail.com>
2022-03-11 18:37:57 +0900

    add: BE sample rspec model test

--- be/config/application.rb
+++ be/config/application.rb
@@ -38,5 +38,19 @@ module Be
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.stylesheets false
+      g.assets false
+      g.template_engine false
+
+      g.test_framework :rspec,
+            request_spec: true,
+            model_spec: true,
+            view_specs: false,
+            helper_specs: false,
+            controller_specs: false,
+            routing_specs: false
+    end
   end
 end
