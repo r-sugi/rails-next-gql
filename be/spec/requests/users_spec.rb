0fb5d28d6e92495df9a703e82971342af785784e
r-sugi <r.sugimoto0615@gmail.com>
2022-03-14 18:23:32 +0900

    add: pre-commit test

--- be/spec/requests/users_spec.rb
+++ be/spec/requests/users_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'UsersResolver' do
     it 'usersが取得できること' do
+
       user1 = create(:user)
       user2 = create(:user)
       resolver = Resolvers::UsersResolver.new(field: nil, object: nil, context:{})
