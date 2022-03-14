f8f23a940ef0e1412835eff91d3400dd75170530
r-sugi <r.sugimoto0615@gmail.com>
2022-03-14 16:32:23 +0900

    add: spec for graphql query, mutations

---
+++ be/spec/requests/update_user_rspec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'UpdateUser', type: :request do
+  describe 'resolver' do
+    it 'userが作成されること' do
+      user = create(:user)
+      create_user_params = {
+        id: user[:id],
+        name: 'hoge2',
+        email: 'sample2@gmail.com'
+      }
+      mutation = Mutations::UpdateUser.new(field: nil, object: nil, context:{})
+      res = mutation.resolve(
+        id: create_user_params[:id],
+        name: create_user_params[:name],
+        email: create_user_params[:email]
+      )
+      expect(res[:user][:id]).to eq create_user_params[:id]
+      expect(res[:user][:name]).to eq create_user_params[:name]
+      expect(res[:user][:email]).to eq create_user_params[:email]
+    end
+  end
+end
---
+++ be/spec/requests/users_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'UsersResolver' do
+    it 'usersが取得できること' do
+      user1 = create(:user)
+      user2 = create(:user)
+      resolver = Resolvers::UsersResolver.new(field: nil, object: nil, context:{})
+      ret = resolver.resolve
+      expect(ret).to eq [user1, user2]
+    end
+  end
+end
