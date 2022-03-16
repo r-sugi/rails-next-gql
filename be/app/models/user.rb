1dd5563b5851098631a6c5eedca8f556ec48a43d
r-sugi <r.sugimoto0615@gmail.com>
2022-03-16 17:44:56 +0900

    add: validates for phone_number

--- be/app/models/user.rb
+++ be/app/models/user.rb
@@ -1,3 +1,8 @@
 # frozen_string_literal: true
 class User < ApplicationRecord
+  validates :phone_number, format: { with: /¥A0¥d{9,10}¥z/ }
+
+  def mobile_phone_number?
+    phone_number.match?(/¥A0[7-9]0¥d{8}¥z/)
+  end
 end
