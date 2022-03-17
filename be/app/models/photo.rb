36af074fec5237193d46b357fa58d98a72a29345
r-sugi <r.sugimoto0615@gmail.com>
2022-03-18 02:51:27 +0900

    rafact: sample code for concern

--- be/app/models/photo.rb
+++ be/app/models/photo.rb
@@ -1,23 +1,4 @@
 # frozen_string_literal: true
 class Photo < ApplicationRecord
-  has_many :taggings, class_name: "PhotoTagging", dependent: :destroy
-  has_many :tags, through: :taggings
-
-  # photo_taggingsテーブルのphoto_idとtag_idカラムに複合ユニーク制約、tagsテーブルのnameカラムに
-  # ユニーク制約が設定されている前提の実装であることに注意
-  def self.tagged_with(tag, *others, any: false)
-    tags = [tag, *others]
-
-    joins(:tags)
-      .merge(reflect_on_association(:tags).klass.with_name(tags))
-      .merge(
-        if tags.size == 1
-          all
-        elsif any
-          distinct
-        else
-          group(primary_key).having("COUNT(*) = ?", tags.size)
-        end
-    )
-  end
+  include Taggable
 end
--- be/app/models/tag.rb
+++ be/app/models/tag.rb
@@ -2,4 +2,6 @@
 class Tag < ApplicationRecord
   has_many :taggings, class_name: "PhotoTagging", dependent: :destroy
   has_many :photos, through: :taggings
+
+  scope :with_name, -> (tags) { where(name: tags.pluck(:name)) }
 end
