23d65e346dc8f2fd58601428c853105acca77775
r-sugi <r.sugimoto0615@gmail.com>
2022-03-17 20:15:57 +0900

    add: UserRegistrationForm use case (register user and send email)

--- be/app/models/user.rb
+++ be/app/models/user.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 class User < ApplicationRecord
+  has_secure_password
+
   composed_of :phone_number, mapping: %w(phone_number value)
+
+  validates :name, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :password, format: { with: /\A[\p{ascii}&&[^\x20]]{8,72}\z/, allow_nil: true }
+  validates :terms_of_service, acceptance: true
+
+  after_create_commit :send_welcome_email
+
+  private
+
+  def send_welcome_email
+    UserMailer.with(user: self).welcome.deliver_later
+  end
 end
--- be/db/schema.rb
+++ be/db/schema.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_17_043601) do
+ActiveRecord::Schema.define(version: 2022_03_17_103944) do
 
   create_table "bank_accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.decimal "balance", precision: 19, scale: 4, null: false
@@ -24,8 +23,11 @@ ActiveRecord::Schema.define(version: 2022_03_17_043601) do
     t.string "name", null: false
     t.string "email"
     t.string "phone_number"
+    t.string "password_digest"
+    t.boolean "terms_of_service"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
