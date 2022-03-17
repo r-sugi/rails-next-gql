153744663be6947c4311de253cbc33c52206dd47
r-sugi <r.sugimoto0615@gmail.com>
2022-03-17 20:24:39 +0900

    refact: UserRegistrationForm use case (register user and send email)

---
+++ be/app/models/forms/user_registration_form.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+class UserRegistrationForm
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  attribute :email, :string
+  attribute :password, :string
+  attribute :password_confirmation, :string
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validate :email_is_not_taken_by_another
+  validates :password, format: { with: /\A[\p{ascii}&&[^\x20]]{8,72}\z/ }, confirmation: { allow_blank: true }
+  validates :terms_of_service, acceptance: { allow_nil: false }
+
+  def save
+    return false if invalid?
+    user.save!
+    UserMailer.with(user: user).welcome.deliver_later
+    true
+  end
+
+  def user
+    @user ||= User.new(email: email, password: password)
+  end
+
+  private
+
+  def email_is_not_taken_by_another
+    errors.add(:email, :taken, value: email) if User.exists?(email: email)
+  end
+end
--- be/app/models/user.rb
+++ be/app/models/user.rb
@@ -7,13 +7,4 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
   validates :password, format: { with: /\A[\p{ascii}&&[^\x20]]{8,72}\z/, allow_nil: true }
-  validates :terms_of_service, acceptance: true
-
-  after_create_commit :send_welcome_email
-
-  private
-
-  def send_welcome_email
-    UserMailer.with(user: self).welcome.deliver_later
-  end
 end
