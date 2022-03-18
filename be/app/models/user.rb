cc9d5020036ad4448610d57b57117816ffc69e4e
r-sugi <r.sugimoto0615@gmail.com>
2022-03-19 05:19:08 +0900

    refact: validates, spec

--- be/app/models/user.rb
+++ be/app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   composed_of :phone_number, mapping: %w(phone_number value)
 
   validates :name, presence: true
-  validates :email, email: true, uniqueness: true
+  validates :email, email: true, uniqueness: { case_sensitive: true }
   validates :password, format: { with: /\A[\p{ascii}&&[^\x20]]{8,72}\z/, allow_nil: true }
 
   # define callbacks by callback_object when loaded
--- be/spec/models/user_spec.rb
+++ be/spec/models/user_spec.rb
@@ -2,10 +2,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before do
-    @user = create(:user)
-  end
-  it 'userインスタンスが有効' do
-    expect(@user).to be_valid
-  end
+  subject(:user) { create(:user) }
+
+  it { is_expected.to be_valid }
 end
