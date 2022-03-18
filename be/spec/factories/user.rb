a2f89e2fd0201f1d8491bb8f2aaeab87a985d7d2
r-sugi <r.sugimoto0615@gmail.com>
2022-03-19 03:42:14 +0900

    fix: failed specs

--- be/spec/factories/user.rb
+++ be/spec/factories/user.rb
@@ -3,6 +3,9 @@ FactoryBot.define do
   factory :user do
     sequence(:id) { |n| n }
     name { "sample_name" }
-    email { "sample@gmail.com" }
+    sequence(:email) { |n| "sample_#{n}@gmail.com" }
+    phone_number { PhoneNumber.new("09011112222") }
+    password { "test_password" }
+    terms_of_service { nil }
   end
 end
