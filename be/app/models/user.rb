7f2b44aa62c293ec6bc7757f7f8f6abf4fdf7c90
r-sugi <r.sugimoto0615@gmail.com>
2022-03-18 14:01:27 +0900

    add: sample code for callback_objcet

--- be/app/models/user.rb
+++ be/app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, email: true, uniqueness: true
   validates :password, format: { with: /\A[\p{ascii}&&[^\x20]]{8,72}\z/, allow_nil: true }
+
+  # define callbacks by callback_object when loaded
+  AttributeEncryptionCallbacks.new(:phone_number).tap do |callbacks|
+    after_find callbacks
+    before_save callbacks
+    after_save callbacks
+  end
 end
