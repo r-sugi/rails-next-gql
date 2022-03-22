8d5a2710d0fad8a23ad493a27769276a1674229a
r-sugi <r.sugimoto0615@gmail.com>
2022-03-22 15:42:04 +0900

    remove: sessionsController

--- be/app/controllers/application_controller.rb
+++ be/app/controllers/application_controller.rb
@@ -1,24 +1,3 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::API
-  def current_user
-    if auth_present?
-      user = User.find(auth["user"])
-      if user
-        @current_user ||= user
-      end
-    end
-  end
-
-  private
-    def token
-      request.env["HTTP_AUTHORIZATION"].scan(/Bearer(.*)$/).flatten.last
-    end
-
-    def auth
-      Auth.decode(token)
-    end
-
-    def auth_present?
-      !!request.env.fetch("HTTP_AUTHORIZATION", "").scan(/Bearer/).flatten.first
-    end
 end
--- be/config/routes.rb
+++ be/config/routes.rb
@@ -7,6 +7,4 @@ Rails.application.routes.draw do
 
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  post "/login", to: "sessions#create"
 end
